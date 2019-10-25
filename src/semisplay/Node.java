097d2a39f33274b7415f98de4027dcea219d4cbd
Lucas Belpaire <lucas.belpaire@ugent.be>
2019-10-25 10:23:23 +0200

    made class package private

--- src/semisplay/Node.java
+++ src/semisplay/Node.java
@@ -1,31 +1,31 @@
 package semisplay;
 
-public class Node<E extends Comparable<E>> {
+class Node<E extends Comparable<E>> {
     private Node<E> parent, leftChild, rightChild;
     private int whichChild; // 0 = root, 1 = leftChild, 2 = rightChild
     private E key;
 
-    public Node(E value){
+    Node(E value){
         this.key = value;
     }
 
-    public Node<E> getLeftChild() {
+    Node<E> getLeftChild() {
         return leftChild;
     }
 
-    public void setLeftChild(Node leftChild) { this.leftChild = leftChild; }
+    void setLeftChild(Node<E> leftChild) { this.leftChild = leftChild; }
 
-    public Node<E> getRightChild() {
+    Node<E> getRightChild() {
         return rightChild;
     }
 
-    public void setRightChild(Node rightChild) { this.rightChild = rightChild; }
+    void setRightChild(Node<E> rightChild) { this.rightChild = rightChild; }
 
-    public Node<E> getParent() { return parent; }
+    Node<E> getParent() { return parent; }
 
-    public void setParent(Node parent) { this.parent = parent; }
+    void setParent(Node<E> parent) { this.parent = parent; }
 
-    public E getKey(){ return key; }
+    E getKey(){ return key; }
 
     /**
      * whichChild is an int value which indicates the state of the node in relation to its parent.
@@ -35,7 +35,7 @@ public class Node<E extends Comparable<E>> {
      * 2: the node is the rightChild of its parent
      * @return int value, between 0 and 3.
      */
-    public int getWhichChild() { return whichChild; }
+    int getWhichChild() { return whichChild; }
 
     /**
      * whichChild is an int value which indicates the state of the node in relation to its parent.
@@ -45,5 +45,5 @@ public class Node<E extends Comparable<E>> {
      * 2: the node is the rightChild of its parent.
      * @param whichChild, sets the state of the node. Value should be 0,1,2.
      */
-    public void setWhichChild(int whichChild) { this.whichChild = whichChild; }
+    void setWhichChild(int whichChild) { this.whichChild = whichChild; }
 }
